6f32a89460c0cefad31c808ca8f5affbfdb33c77
장혜진 <jang314@naver.com>
2024-06-03 13:23:46 +0900

    chapter8 학습 진행중

---
+++ src/chapter08/HeapElement.java
@@ -0,0 +1,6 @@
+package chapter08;
+
+public class HeapElement<T> {
+    private T data;
+    private int pr;
+}
---
+++ src/chapter08/SimpleHeap.java
@@ -0,0 +1,52 @@
+package chapter08;
+
+import java.lang.reflect.Array;
+
+public class SimpleHeap<T> extends Heap<T> {
+    private HeapElement[] heapArray;
+
+    public SimpleHeap(Class cls) {
+        super();
+        heapArray = (HeapElement[]) Array.newInstance(cls, DEFAULT_ARRAY_SIZE);
+    }
+
+    @Override
+    public boolean hIsEmpty() {
+        return numOfData == 0;
+    }
+
+    @Override
+    public void hInsert(T data, int priority) {
+
+    }
+
+    @Override
+    public T hDelete() {
+        return null;
+    }
+
+    @Override
+    public int getParentIdx(int idx) {
+        return idx / 2;
+    }
+
+    @Override
+    public int getLChildIdx(int idx) {
+        return idx * 2;
+    }
+
+    @Override
+    public int getRChildIdx(int idx) {
+        return (idx * 2) + 1;
+    }
+
+    /**
+     *  두 개의 자식 노드 중 높은 우선순위의 자식 노드 인덱스 값 반환
+    * */
+    @Override
+    public int getHiPriChildIdx(int idx) {
+        int lChildIdx = getLChildIdx(idx);
+
+        return 0;
+    }
+}
